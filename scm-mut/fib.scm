dd8029f07ff7f2f50081302272b7f233ea36fa8f
Quentin Stievenart <quentin.stievenart@gmail.com>
2014-11-04 13:40:03 +0100

    Add example with mutation

---
+++ scm-mut/fib.scm
@@ -0,0 +1,12 @@
+(let ((fib (lambda (n)
+             (let ((a 0)
+                   (b 1))
+               (letrec ((loop (lambda (x)
+                                (if (= x n)
+                                    a
+                                    (let ((olda a))
+                                      (set! a b)
+                                      (set! b (+ olda b))
+                                      (loop (+ x 1)))))))
+                 (loop 0))))))
+  (fib 8))
