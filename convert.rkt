6a1c59425673aefa8493f4cb9194e47766b490bc
Quentin Stievenart <quentin.stievenart@gmail.com>
2014-11-17 17:04:17 +0100

    Add some unit tests

--- convert.rkt
+++ convert.rkt
@@ -8,6 +8,7 @@
 ;;   - TODO: incorrectly rewrites lambdas with more than one expression inside
 
 (require racket/match)
+(require rackunit)
 
 ;; '(1 2 3) -> '((1 2) . 3
 (define (split-last l)
@@ -16,29 +17,48 @@
       (match (split-last (cdr l))
         [(cons rest last) (cons (cons (car l) rest) last)])))
 
+(check-equal? (split-last '(1 2 3)) '((1 2) . 3))
+
 (define (atom? exp)
   (not (pair? exp)))
+
+(check-true (atom? 'foo))
+(check-false (atom? '(foo)))
 
 ;; true for expressions that don't need to extract definitions to become atomic
 (define (extract-free? exp)
   (or (atom? exp) (equal? (car exp) 'lambda) (equal? (car exp) 'quote)
       (equal? (car exp) 'let) (equal? (car exp) 'letrec) (equal? (car exp) 'let*)))
 
+(check-true (extract-free? 'foo))
+(check-true (extract-free? '(lambda (x) x)))
+(check-true (extract-free? '(lambda (x) (+ (* x 2))))) ;; should be ANFized inside the lambda
+(check-true (extract-free? '(let ((x 1)) (+ (* x 2))))) ;; same for lets
+(check-false (extract-free? '(+ (* x 2))))
+
 ;; insert-in (let ((x 0)) __) x -> (let ((x 0)) x)
 (define (insert-in e1 e2)
   (cond
    ((equal? e1 '__) e2)
-   ((or (equal? (car e1) 'let) (equal? (car e1) 'letrec) (equal? (car e1) 'let*))
-    `(,(car e1) ,(cadr e1) ,(insert-in (caddr e1) e2)))))
+   ((and (pair? e1) (or (equal? (car e1) 'let) (equal? (car e1) 'letrec) (equal? (car e1) 'let*)))
+    `(,(car e1) ,(cadr e1) ,(insert-in (caddr e1) e2)))
+   (else e1)))
 
-;; (insert-in '(let ((x 0)) __) 'x)
-;; (insert-in '(let ((x (* 2 3))) (let ((y (+ x 1))) __)) 'y)
-;; (insert-in (insert-in '(let ((x 0)) __) '(let ((y (+ x 1))) __)) 'y)
+(check-equal? (insert-in '(let ((x 0)) __) 'x) '(let ((x 0)) x))
+(check-equal? (insert-in '(let ((x 0)) x) 'y) '(let ((x 0)) x))
+(check-equal? (insert-in '(let ((x (* 2 3))) (let ((y (+ x 1))) __)) 'y)
+              '(let ((x (* 2 3))) (let ((y (+ x 1))) y)))
+(check-equal? (insert-in (insert-in '(let ((x 0)) __) '(let ((y (+ x 1))) __)) 'y)
+              '(let ((x 0)) (let ((y (+ x 1))) y)))
 
 (define id 0)
 (define (newid)
   (set! id (+ id 1))
   (string->symbol (string-append "_" (number->string id))))
+
+(set! id 0)
+(check-equal? (newid) '_1)
+(check-equal? (newid) '_2)
 
 ;; (set! x (+ x 1)) -> [(let ((_x0 (+ x 1))) __) _x0]
 ;; ((id f) (id x)) -> [(let ((_idf (id f))) (let ((_idx (id x))) (let (_call (_idf _idx))))) _call]
@@ -66,10 +86,12 @@
          (cons (insert-in defs `(let ((,var ,(reverse rev-vars))) __))
                var))]))))
 
-;; (extract-defs '(set! x (+ x 1)))
-;; (extract-defs '(let ((x (set! x 1))) y))
-;; (extract-defs '(if (> x 1) a b))
-
+(set! id 0)
+(check-equal? (extract-defs '(set! x (+ x 1))) '((let ((_1 (+ x 1))) __) . (set! x _1)))
+(check-equal? (extract-defs '((id f) (id x))) '((let ((_2 (id f))) (let ((_3 (id x))) (let ((_4 (_2 _3))) __))) . _4))
+(check-equal? (extract-defs '(let ((x (set! x 1))) y)) '(__ . (let ((x (set! x 1))) y)))
+(check-equal? (extract-defs '(if (> x 1) a b)) '((let ((_5 (> x 1))) __) . (if _5 a b)))
+(check-equal? (extract-defs '(let ((x (set! (+ x 1)))) y)) '((let ((_6 (+ x 1))) __) . (let ((x (set! x _6))) y)))
 
 ;; Only works with a bunch of defines followed by one main expression
 ;; '((define x 1) foo) -> (letrec ((x 1)) foo)
@@ -82,6 +104,11 @@
       (if (= (length exp) 1)
           (car exp)
           `(begin ,@exp))))
+
+(check-equal? (remove-defines '((define x 1) x)) '(let ((x 1)) x))
+(check-equal? (remove-defines '((define (id x) x) (id 3))) '(letrec ((id (lambda (x) x))) (id 3)))
+(check-equal? (remove-defines '((define a 1) (define b 2) (define (c x) 3) (+ a b (c 1))))
+              '(let ((a 1)) (let ((b 2)) (letrec ((c (lambda (x) 3))) (+ a b (c 1))))))
 
 ;; (quote foo) -> (quote foo)
 ;; (quote (foo bar (baz))) -> (cons 'foo (cons 'bar (cons (cons 'baz '()) '())))
@@ -97,14 +124,28 @@
         (loop (cadr exp)))
       exp))
 
-;; (cond (cnd1 e1 e2) (else e3)) -> (if cnd1 (begin e1 e2) e3)
-;; (cond (cnd1 e1) (cnd2 e2) (else e3)) -> (if cnd1 e1 (if cnd2 e2 e3))
+(check-equal? (simplify-quote ''foo) ''foo)
+(check-equal? (simplify-quote ''(foo bar (baz)))
+              '(cons 'foo (cons 'bar (cons (cons 'baz '()) '()))))
+(check-equal? (eval (simplify-quote ''(foo bar (baz)))) '(foo bar (baz)))
+(check-equal? (eval (simplify-quote ''((foo) (bar (baz (qux))) (baz (bar) (qux quux)))))
+              '((foo) (bar (baz (qux))) (baz (bar) (qux quux))))
+(check-equal? (simplify-quote '(if (> x 1) a b)) '(if (> x 1) a b))
+
+(define (make-begin exps)
+  (if (= (length exps) 1)
+      (car exps)
+      `(begin ,@exps)))
+
+(check-equal? (make-begin '(foo)) 'foo)
+(check-equal? (make-begin '(foo bar)) '(begin foo bar))
+
 (define (remove-cond exp)
   (if (and (pair? exp) (equal? (car exp) 'cond))
       (if (equal? (caadr exp) 'else)
-          `(begin ,@(cdadr exp))
+          (make-begin (cdadr exp))
           `(if ,(caadr exp)
-               (begin ,@(cdadr exp))
+               ,(make-begin (cdadr exp))
                ,(remove-cond
                  (if (pair? (cddr exp))
                      `(cond ,@(cddr exp))
@@ -113,8 +154,14 @@
                        #f)))))
       exp))
 
-;; (let ((x 1) (y 2)) body) -> (let ((x 1)) (let ((y 2)) (begin body)))
-;; Won't preverve semantics in the presence of mutual recursion
+(check-equal? (remove-cond '(cond ((= x 1) a) ((= x 2) b) ((= x 3) c) (else d)))
+              '(if (= x 1) a (if (= x 2) b (if (= x 3) c d))))
+(check-equal? (remove-cond '(cond ((= x 1) a b c) (else d e f)))
+              '(if (= x 1) (begin a b c) (begin d e f)))
+
+;; Won't preverve semantics in the presence of mutual recursion. To use
+;; mutual-recursion, the input program has to be modified to explicitely do a
+;; let/define followed by a set!
 (define (simplify-lets exp)
   (if (pair? exp)
       (cond
@@ -124,14 +171,21 @@
                               (insert-in acc `(,sym ((,(car binding) ,(simplify-lets (cadr binding)))) __)))
                             '__
                             (cadr exp))
-                     (if (= (length (cddr exp)) 1)
-                         (caddr exp)
-                         `(begin ,@(cddr exp))))))
+                     (make-begin (cddr exp)))))
        ((equal? (car exp) 'let*)
         (simplify-lets (cons 'let (cdr exp))))
        (else
         exp))
       exp))
+
+;; (let ((x 1) (y 2)) body) -> (let ((x 1)) (let ((y 2)) (begin body)))
+(check-equal? (simplify-lets '(let ((x 1) (y 2)) body)) '(let ((x 1)) (let ((y 2)) body)))
+(check-equal? (simplify-lets '(let ((x 1) (y 2)) a b)) '(let ((x 1)) (let ((y 2)) (begin a b))))
+(check-equal? (simplify-lets '(let* ((x 1) (y 2)) body)) '(let ((x 1)) (let ((y 2)) body))) ; get rid of let*
+(check-equal? (simplify-lets '(letrec ((x 1) (y 2)) body)) '(letrec ((x 1)) (letrec ((y 2)) body)))
+;; TODO:
+;; (check-equal? (simplify-lets '(letrec ((x (lambda () (y))) (y (lambda () (x)))) body)
+;;               '(let ((x #f)) (let ((y (lambda () (x)))) (begin (set! x (lambda () (y))) body))))
 
 (define (to-anf exp)
   (cond
